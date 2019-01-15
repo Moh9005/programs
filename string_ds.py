fedbcc273feec49f2e989922805d55980a9410b5
Muhammad-Mohsin-Arain <mohsin9005@hotmail.com>
2019-01-15 14:18:30 +0500

    all python files

--- string_ds.py
+++ string_ds.py
@@ -1,20 +1,20 @@
 a=input("enter string # 1 ")
 b=input("enter string # 2 ")
-n=len(a)-1
-x=len(b)-1
+n=len(a)
+x=len(b)
 c=[]
 global m
 m=0
 i=j=0
-while (i!=n and j!=x):
-    if(a[i]== b[j]):
-       c.append(a[i])
-       i=i+1
-       m=m+1
-       print('output = yes ')
-    elif(a[i]!= b[j]):
-        j=j+1
-        i=i+1
+for i in range (n):
+    for j in range (x):
+        # print(b[j], c[l] ,j,l)
+        if a[i]==b[j]:
+            # c[l]= ' '
+            m +=1
+print(c)
+
 if(m==0):
     print("no match found")
-
+else:
+    print("match found")
